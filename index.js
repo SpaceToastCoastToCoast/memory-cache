4b2ef9795aa98baa85e8f876f94e658490c172b6
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-09-22 09:50:24 -1000

    added a timeout to the test to compensate for load lag on page

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-console.time("non-memoized call");
+setTimeout(function() {
+console.time("non-memoized call getById");
 var findMe = memoizeGetElementById("findMe");
-console.timeEnd("non-memoized call");
+console.timeEnd("non-memoized call getById");
 
 findMe.style.color = "blue";
 
-console.time("memoized call");
+console.time("memoized call getById");
 var findMeAgain = memoizeGetElementById("findMe");
-console.timeEnd("memoized call");
+console.timeEnd("memoized call getById");
 
 findMeAgain.style.fontWeight = "bold";
 
-console.time("non-memoized call");
+console.time("non-memoized call query");
 var alsoFindMe = memoizeQuerySelector("#alsoFindMe");
-console.timeEnd("non-memoized call");
+console.timeEnd("non-memoized call query");
 
 alsoFindMe.style.color = "red";
 
-console.time("memoized call");
+console.time("memoized call query");
 var alsoFindMeAgain = memoizeQuerySelector("#alsoFindMe");
-console.timeEnd("memoized call");
+console.timeEnd("memoized call query");
 
-alsoFindMeAgain.style.textAlign = "center";
+alsoFindMeAgain.style.textAlign = "center"; }, 1000);
